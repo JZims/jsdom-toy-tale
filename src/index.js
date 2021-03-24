ebafd300d741d9501ef0ea425ca226f931992b67
JZims <JZimmsGames@gmail.com>
2021-03-24 11:11:24 -0400

    working along..

--- src/index.js
+++ src/index.js
@@ -13,3 +13,49 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+ 
+
+const toyButton = document.querySelector("#new-toy-button")
+const toyCollection = document.querySelector("#toy-collection")
+const toyItemDiv = document.createElement("div")
+      toyItemDiv.classList.add("card")
+const toyImage = document.createElement("img")
+      toyImage.classList.add("toy-avatar")
+const toyLikes = document.createElement("p")
+      toyLikes.classList.add("likes")
+const toyCardButton = document.createElement("button")
+      toyCardButton.classList.add("like-btn")
+//const toyName = document.querySelector("")
+
+
+fetch('http://localhost:3000/toys', {
+  method: "GET", 
+  headers: {
+      "Content-Type": "application/json", 
+      Accept: "application/json"
+  }, 
+  body: JSON.stringify({
+      
+      id: 0, 
+      name: ,
+      image: toyImage,
+      likes: 0
+
+  })  
+}
+
+  .then(res => res.json)
+  .then(json => {
+    turnURLtoHTML(json)
+    console.log("hello")
+    debugger;
+  })
+  
+)
+
+function turnURLtoHTML(toyData){
+
+  
+}
+
