ecec6eb75a5a41472dd109d187d3e92959e9bdae
JZims <JZimmsGames@gmail.com>
2021-03-24 13:27:42 -0400

    Got cards to populate, need to make images work

--- src/index.js
+++ src/index.js
@@ -18,31 +18,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const toyButton = document.querySelector("#new-toy-button")
 const toyCollection = document.querySelector("#toy-collection")
-const toyItemDiv = document.createElement("div")
-      toyItemDiv.classList.add("card")
-const toyImage = document.createElement("img")
-      toyImage.classList.add("toy-avatar")
-const toyLikes = document.createElement("p")
-      toyLikes.classList.add("likes")
-const toyCardButton = document.createElement("button")
-      toyCardButton.classList.add("like-btn")
-const toyName = document.createElement("h2")
+
+
+
       
 
 fetch('http://localhost:3000/toys')
 
-  .then(res => res.json)
-  .then(function (json){
-    json.forEach (function(json){
-      turnURLtoHTML(json)
-    })
-    })
+  .then(res => res.json())
+  .then(function(data){
+    for (var i in data){
+      turnURLtoHTML(data[i])
+    }
+  }
+  )
+    
+
+function turnURLtoHTML(toyData){
+
+  const toyItemDiv = document.createElement("div")
+        toyItemDiv.classList.add("card")
+
+  const toyName = document.createElement("h2")
+        toyName.innerText=`${toyData.name}`
+        toyItemDiv.append(toyName)
+
+  const toyImage = document.createElement("img")
+        toyImage.classList.add("toy-avatar")
+        toyItemDiv.append(toyImage)
+        toyImage.innerText= `src="${toyData.image}"`
+
+  const toyLikes = document.createElement("p")
+        toyLikes.classList.add("likes")
+        toyLikes.innerText = `${toyData.likes} Like(s)`
+        toyItemDiv.append(toyLikes)
+
+  const toyCardButton = document.createElement("button")
+        toyCardButton.classList.add("like-btn")
+        toyCardButton.innerText= "Like <3"
+        toyItemDiv.append(toyCardButton)
 
   
 
-
-function turnURLtoHTML(toyData){
-    toyName.innerText=`${toyData.name}`
-    toyImage.innerHTML=`src = "${toyData.image}"`
+  toyCollection.append(toyItemDiv)
     
 }
